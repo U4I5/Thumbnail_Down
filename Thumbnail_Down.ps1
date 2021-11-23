7ca497f8fd575b35524e71e6703a9dc3912467b8
U4I5 <u.soglondey@gmail.com>
2021-11-23 09:25:14 -0500

    Upload File

---
+++ Thumbnail_Down.ps1
@@ -0,0 +1,10 @@
+
+# Thumbnails Downloader
+
+$id = Read-Host "Video id "
+
+$url = "https://img.youtube.com/vi/$id/maxresdefault.jpg"
+
+
+
+Invoke-WebRequest $url -OutFile C:\Users$env:UserName\Pictures\Uridesign\test.jpg
