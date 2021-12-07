10e19f965fd63a95ef73576c831df23ae0171bc0
U4I5 <92850587+U4I5@users.noreply.github.com>
2021-12-07 08:50:11 +0100

    Update Thumbnail_Down.ps1

--- Thumbnail_Down.ps1
+++ Thumbnail_Down.ps1
@@ -5,6 +5,4 @@ $id = Read-Host "Video id "
 
 $url = "https://img.youtube.com/vi/$id/maxresdefault.jpg"
 
-
-
-Invoke-WebRequest $url -OutFile C:\Users$env:UserName\Pictures\Uridesign\test.jpg
+Invoke-WebRequest $url -OutFile C:\Users$env:UserName\Pictures\Uridesign\test.jpg #path of destination file
